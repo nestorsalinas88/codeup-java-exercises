ea2ea5326e00e0346e9cc0aee4c48258b91dbfb0
nestorsalinas88 <nestorasm88@gmail.com>
2018-04-30 16:10:57 -0500

    refactored do / while functionality

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 2;
+//        int i = 2;
 //        while (i <= 15){
 //            System.out.println("i is " + i);
 //            i++;
 //
 //        }
 
-        do {
+        for(int i = 0; i <= 10; i++)
+        {
             System.out.println("i is " + i);
-            i *= 2;
-        } while (i <= 1000000);
+
+        }
 
     }
 }
