12f0b633c01642d81ea70648b6548b374d2350dd
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-01 09:57:29 -0500

    Created a loop that will iterate through some conditional statements in order to find specifics

--- src/Bob.java
+++ src/Bob.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-        Scanner
+        String message;
+        String name = "bob";
+        Scanner sc = new Scanner(System.in);
+
+        boolean match;
+
+        System.out.println("Sup dude? (type exit, to quit)");
+
+        do {
+            match = false;
+            System.out.println("> ");
+            message = sc.nextLine();
+
+            if(message.contains("!")) {
+                System.out.println("Whoa, chill out!");
+                match = true;
+            }
+
+
+        } while (!message.contains("exit"));
+
+
+
+
     }
 }
