75b1a026d25264a81c66dd089d5aeb732cee057c
nestorsalinas88 <nestorasm88@gmail.com>
2018-04-30 23:57:55 -0500

    practice loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,85 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for(int i = 2; i <= 15; i++){
-            System.out.println("i is " + i);
 
-        }
+
+
+//        String fuzz = "fuzz", buzz = "buzz";
+//        for(int i = 0; i <= 100; i++){
+//            System.out.println(i);
+//            if(i % 3 == 0){
+//                System.out.println(fuzz + i);
+//            }
+//            if(i % 5 == 0){
+//                System.out.println(buzz + i);
+//            }
+//            if(i % 15 == 0){
+//                System.out.println(fuzz + buzz + i);
+//            }
+//        }
+
+//        for(int i = 2; i <= 15; i++){
+//            System.out.println("i is " + i);
+//
+//        }
 //
 //        for(int i = 0; i <= 10; i++)
 //        {
 //            System.out.println("i is " + i);
 //
 //        }
+
+//        String buzz = "buzz", fizz = "fizz";
+//
+//        for(int i = 1; i <= 100; i++){
+//            System.out.println(i);
+//            if (i % 15 == 0){
+//                System.out.println(fizz + buzz + i);
+//            } else if (i % 3 == 0){
+//                System.out.println(buzz + i);
+//            }else if(i % 5 == 0) {
+//                System.out.println(fizz + i);
+//            }
+//        }
+
+
+        int userInput;
+        Scanner lg = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        userInput = lg.nextInt();
+
+        System.out.println("Number\tCubed\tSquared");
+//
+//        System.out.println(userInput);
+
+        for (int i = 1; i <= userInput; i++){
+            int square = i * i;
+            int cubed = i * i * i;
+            System.out.println(i + "\t" + cubed + "\t" + square);
+
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to continue? [Y/N]");
+
+        String hey = sc.nextLine();
+
+        boolean yes = hey.equalsIgnoreCase("Y");
+        boolean no = hey.equalsIgnoreCase("N");
+
+        if (yes == true){
+            System.out.println( "Lets continue");
+        }
+        if (no == true){
+            System.out.println( "Lets Stop then");
+        }
+
+
+
+
 
     }
 }
