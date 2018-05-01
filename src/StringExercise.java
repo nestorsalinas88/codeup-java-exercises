5fa61fe0e07e33423593d21879816b534ed7aeac
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-01 07:18:54 -0500

    creeated: new class, String message

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education";
+
+        System.out.println(message);
+
+    }
+}
