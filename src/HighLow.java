a2e8d5005456b463f802caecfd4da0582f8c5d69
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-02 15:30:17 -0500

    created: integers and boolean

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class HighLow {
+
+    public static void main(String[] args) {
+        int randomNumber = roll(100);
+        boolean matched = false;
+        int guess;
+        int i = 1;
+
+    }
 }
