a3863622e47e6b2051f64822db954800e47b2018
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-02 08:51:13 -0500

    completed simply add function

---
+++ src/MethodsExercises.java
@@ -0,0 +1,22 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println("add: 3 + 4 = " + add(3,4));
+    }
+
+    public  static int add(int a, int b){
+    return a + b;
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+    public  static float divide(int a, int b){
+        return (float) a / b;
+    }
+
+
+}
