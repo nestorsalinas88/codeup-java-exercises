11217a364c24c7213811d52aa0c78e3ea325dc66
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-02 15:49:47 -0500

    create: a recursion method

--- src/HighLow.java
+++ src/HighLow.java
@@ -13,5 +13,27 @@ public class HighLow {
         System.out.println("Guess my number dude! ");
 
 
+        do {
+            System.out.print("Guess > ");
+            guess = sc.nextInt();
+
+            if(guess < randomNumber){
+                System.out.println("TOO LOW DUDE ");
+
+            } else if (guess > randomNumber){
+                System.out.println("Too high");
+            } else  {
+                System.out.println("You're good");
+                System.out.println("Took you this many time : " + i );
+                matched = true;
+            }
+            i++;
+            if(i == 20){ matched = true;
+                System.out.println("You suck");}
+        } while (!matched);
+
+    }
+    public static int roll(int max){
+        return (int)(Math.random() * max) + 1;
     }
 }
