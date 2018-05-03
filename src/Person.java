1dce82165970a6747e054fa3026f9b13b9f57fc8
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-03 15:33:53 -0500

    created: main method to print names

--- src/Person.java
+++ src/Person.java
@@ -19,6 +19,16 @@ public class Person {
 
 
 
+        public static void main(String[] args) {
 
+            Person person1 = new Person("John");
+            Person person2 = person1;
+            System.out.println(person1.getName());
+            System.out.println(person2.getName());
+            person2.setName("Jane");
+            System.out.println(person1.getName());
+            System.out.println(person2.getName());
+
+        }
 
 }
