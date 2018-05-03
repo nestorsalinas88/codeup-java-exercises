021ee29db0dccfa4085f2d6cc18c86a3e898e5b1
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-03 14:50:58 -0500

    created: userapp class

--- src/UserApplication.java
+++ src/UserApplication.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class UserApplication {
+    public static void main(String[] args) {
+
+        System.out.println("welcome to my app");
+        System.out.println("Currently, we have " + User.getNumberOfUsers() + " of users");
+
+        User luis = new User();
+        luis.email = "Luis@gmail.com";
+        luis.username = "luis33";
+        luis.password = "password123";
+        luis.isAdmin = true;
+
+        System.out.println(luis.username + "'s email address is " + luis.email);
+
+        User nestor = new User();
+        nestor.email = "yahoo";
+        nestor.username = "ne12";
+        nestor.password = "kjhasd";
+        nestor.isAdmin = false;
+        System.out.println(nestor.username);
+
+    }
 }
