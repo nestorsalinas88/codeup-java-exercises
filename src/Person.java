dfd7736c142f946cd0fe8c842fcde36ec3273be9
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-03 14:00:50 -0500

    completed: first field exercise

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,14 @@
 public class Person {
+    public String firstName;
+    public String lastName;
 
+
+//    instance method
+    public String fullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public String yellName(){
+        return firstName.toUpperCase();
+    }
 }
