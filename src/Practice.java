e29c8d90b9d30759b8a9f152bb4483c00a2d1264
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-02 22:10:00 -0500

    test: testing basic knowledge of method overloading

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Practice {
+//    visibility (static) returnType methodName (paramType1 param 1, paramType2 param 2)
+
+
+        public static void main(String s[])
+        {
+            print();
+            print(8);
+            print(20 < 10);
+        }
+
+        public static void print()
+        {
+            System.out.println("Called print with no parameters");
+        }
+
+        public static void print(int i)
+        {
+            System.out.println("Called print with int parameter");
+        }
+
+        public static void print(boolean b)
+        {
+            System.out.println("Called print with boolean parameter");
+        }
+
+
 }
+
