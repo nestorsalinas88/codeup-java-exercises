7866c4cd69f4c254604d444d0570f377507f5f6d
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-03 19:53:43 -0500

    refactor: different approach

--- src/Person.java
+++ src/Person.java
@@ -1,21 +1,29 @@
 public class Person {
         private String name;
 
+        // returns the person's name
+        public String getName(){
+            return name;
+        }
+
+
+        // changes the name property to the passed value
+        public void setName(String input){
+            this.name = input ;
+        }
+
+
+        // prints a message to the console using the person's name
+        public void sayHello(){
+            System.out.println("Hello from " + this. name);
+        }
+
+
+
         public Person(String name) {
             this.name = name;
         }
 
-
-        public String getName(){
-            return this.name;
-        }
-        public void setName(String input) {
-            this.name =  input;
-        }
-
-        public void sayHello() {
-            System.out.println("Hello from " + this.name);
-        }
 
 
 
@@ -23,8 +31,6 @@ public class Person {
 
             Person person1 = new Person("John");
             Person person2 = person1;
-            System.out.println(person1.getName());
-            System.out.println(person2.getName());
             person2.setName("Jane");
             System.out.println(person1.getName());
             System.out.println(person2.getName());
