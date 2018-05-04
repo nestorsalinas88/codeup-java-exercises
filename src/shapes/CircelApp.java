2e7e8bf6f9a49372fc679f59adbccb63b5195496
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-04 09:34:16 -0500

    created: method overload creating circle

--- src/shapes/CircelApp.java
+++ src/shapes/CircelApp.java
@@ -1,5 +1,7 @@
 package shapes;
 
+import util.Input;
+
 public class CircelApp {
     private static int circleCount;
 
@@ -7,8 +9,30 @@ public class CircelApp {
         return circleCount;
     }
 
+    static void increaseCircleCount(){
+        circleCount++;
+    }
+
+    static void makeCircle(){
+        System.out.println("What is the radius?");
+        Input input = new Input();
+        double userRadius = input.getDouble();
+        Circle userCircle = new Circle(userRadius);
+        System.out.println("The radius was" + userRadius);
+        System.out.println("Area: " + userCircle.getArea());
+        System.out.println("Circumference: " + userCircle.getCircumference());
+        increaseCircleCount();
+        System.out.println("Do you want to continue? ");
+        boolean toContinue = input.yesNo();
+        if (toContinue) {
+            makeCircle();
+        } else {
+            System.out.println("As you wish, you had made " + getCircleCount() + "!");
+        }
+    }
+
+
     public static void main(String[] args) {
-
-
+        makeCircle();
     }
 }
