d5ae05a15f16b7bd26f8f8038ca3892577fe3cbd
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-03 20:32:59 -0500

    refactor: method overloading

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,52 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner sc;
+
+    public Input(){
+        this.sc = new Scanner(System.in).useDelimiter("\n");
+    }
+    public String getString(){
+        return this.sc.nextLine();
+    }
+
+//    public String getString(String prompt){
+//        System.out.println(prompt);
+//        return getString();
+//    }
+
+//    public boolean pressEnter() {
+//        try
+//        {
+//            System.in.read();
+//        }
+//        catch(Exception e)
+//        {}
+//        return true;
+//    }
+//    public boolean pressEnter(String prompt) {
+//        System.out.print(prompt);
+//        return pressEnter();
+//    }
+
+    public boolean yesNo() {
+        String input = this.sc.nextLine();
+        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+    }
+
+//    public boolean yesNo(String prompt) {
+//        System.out.print(prompt);
+//        return yesNo();
+//    }
+
+//    public int getInt(int min, int max){
+//
+//    }
+//    public int getInt()
+//    public double getDouble(double min, double max)
+//    public double getDouble()
+//
+
 }
