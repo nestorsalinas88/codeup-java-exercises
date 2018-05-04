a5f77d37fa069ba723ab9ee19f2891d480c95991
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-04 09:20:23 -0500

    created: method

--- src/shapes/CircelApp.java
+++ src/shapes/CircelApp.java
@@ -1,7 +1,14 @@
 package shapes;
 
 public class CircelApp {
+    private static int circleCount;
+
+    static int getCircleCount(){
+        return circleCount;
+    }
+
     public static void main(String[] args) {
+
 
     }
 }
