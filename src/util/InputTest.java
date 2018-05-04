5fd773df77ffb5d3a341b1432ba468a74ec2cec6
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-04 01:37:25 -0500

    Created: System Output

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,4 +1,20 @@
 package util;
 
 public class InputTest {
+
+    public static void main(String[] args) {
+
+
+        Input scanner = new Input();
+        System.out.println(scanner);
+
+        System.out.println("Give me a double! " + scanner.getDouble());
+        System.out.println(scanner.getInt("Give me an int! "));
+        System.out.println(scanner.getDouble(1.2, 10.7, "Give me a double!"));
+        System.out.println(scanner.getInt(3, 13, "Give me an int!"));
+        System.out.println(scanner.yesNo("Would you like to continue?"));
+        scanner.getString();
+        System.out.println(scanner.getString("Give me a string! "));
+
+    }
 }
