b67619fa5bf65ad46c2e1518441b08470d962301
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-06 18:32:42 -0500

    chore: creted random generator to combine both arrays

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -3,10 +3,18 @@ public class ServerNameGenerator {
 
     public static void main(String[] args) {
 
+//        created two new arrays
         String [] adjectives = {"big", "small", "gray", "massive", "chubby", "fat","skinny","hot","cold","giant"};
 
 
         String [] nouns = {"cats","dogs","horses","monkeys","rats","lions","pigs","birds","insects","elephants"};
 
+//        The name of the server
+        System.out.println("Here is your server name: ");
+        System.out.println(randomElement(adjectives) + "-" + randomElement(nouns));
+    }
+
+    public static String randomElement(String[] name){
+        return name[(int) (Math.random() * name.length)];
     }
 }
