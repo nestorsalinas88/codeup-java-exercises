3dd535ff29e56f7bba64507436afb251647363ac
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-06 18:27:10 -0500

    created: 2 string array

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,12 @@
+public class ServerNameGenerator {
+
+
+    public static void main(String[] args) {
+
+        String [] adjectives = {"big", "small", "gray", "massive", "chubby", "fat","skinny","hot","cold","giant"};
+
+
+        String [] nouns = {"cats","dogs","horses","monkeys","rats","lions","pigs","birds","insects","elephants"};
+
+    }
+}
