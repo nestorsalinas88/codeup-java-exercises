7f2c030d794e7f3e06d559ae58021abdaad335e6
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-06 18:40:32 -0500

    created: note

--- src/ClassFields.java
+++ src/ClassFields.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class ClassFields {
+
+        // static property
+        //
+        public static double pi = 3.14159;
+
+
+        // static methods
+        public static int add(int x, int y) {
+            return x + y;
+        }
+        public static int multiply(int x, int y) {
+            return x * y;
+        }
+
+
+
 }
