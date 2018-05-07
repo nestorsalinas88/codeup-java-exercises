91f47441fa02e3b849bbdad8fb0afade38cb7b36
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-07 17:50:13 -0500

    refactored: input test methods

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -2,11 +2,43 @@ package util;
 
 public class InputTest {
 
+
+
     public static void main(String[] args) {
 
 
         Input scanner = new Input();
         System.out.println(scanner);
+
+        // Binary Test
+        // Integer Testing
+        System.out.print("Input and a binary number: ");
+        int userInt = scanner.getInt();
+        System.out.println("Int Input: " + userInt);
+
+        // Yes No Testing
+        System.out.println("Input y/yes for true: ");
+        boolean userYes = scanner.yesNo();
+        System.out.println("Int Input: " + userYes);
+
+
+
+        // Integer Testing
+        System.out.println("Input and integer: ");
+        userInt = scanner.getInt();
+        System.out.println("Int Input: " + userInt);
+
+        System.out.println("Input and integer between ");
+        userInt = scanner.getInt(5, 10);
+        System.out.println("Between Int Input: " + userInt);
+
+//
+//        userInt = scanner.getBinary("Input and integer (prompt)");
+//        System.out.println("(prompt) Int Input: " + userInt);
+//
+//        userInt = scanner.getInt("Input an integer between (prompt)",5,10);
+//        System.out.println(userInt);
+
 
         System.out.println("Give me a double! " + scanner.getDouble());
         System.out.println(scanner.getInt("Give me an int! "));
