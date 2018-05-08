c0331e2828e67a3fa5b513aac178be3225ae526e
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 13:43:42 -0500

    chore: added variables and methods

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,24 @@
 package shapes;
 
-public class Quadrilateral extends Shape{
+public abstract class Quadrilateral extends Shape implements Measurable{
+
+    protected int length;
+    protected int width;
+
+    public Quadrilateral(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    abstract void setLength(int input);
+    abstract void setWidth(int input);
 }
