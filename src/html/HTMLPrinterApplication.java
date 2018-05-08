6ff1b17368b9dbb10ec6cca9266ae34f060d9384
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 04:30:05 -0500

    created: html printer app

--- src/html/HTMLPrinterApplication.java
+++ src/html/HTMLPrinterApplication.java
@@ -1,7 +1,21 @@
-package movies.html;
+package html;
 
 public class HTMLPrinterApplication {
     public static void main(String[] args) {
         Paragraph p = new Paragraph("Some text");
+        Span span = new Span("some text");
+        Anchor a = new Anchor("click me");
+
+
+        Element[] elements = {p, span, a};
+        printElements(elements);
     }
+
+    public static void printElements(Element[] elements){
+
+        for(Element element : elements){
+            System.out.println(element.toHTML());
+        }
+    }
+
 }
