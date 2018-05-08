fa3109d8c2ee6bcd3c3d15b3bc833ec7ff846df1
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 11:49:58 -0500

    commented out the previous exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,18 +3,18 @@ package shapes;
 public class Square extends Rectangle {
 
 
-    protected double side;
-
-    public Square(double side){
-        super(side, side);
-        this.side = side;
-    }
-
-    public double getArea(){
-      return this.side * this.side;
-    }
-    public double getPerimeter(){
-      return this.side * 4;
-    }
+//    protected double side;
+//
+//    public Square(double side){
+//        super(side, side);
+//        this.side = side;
+//    }
+//
+//    public double getArea(){
+//      return this.side * this.side;
+//    }
+//    public double getPerimeter(){
+//      return this.side * 4;
+//    }
 
 }
