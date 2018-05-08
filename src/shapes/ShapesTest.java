2dfd534237c632c0d8def2f9711d2fd048ffe361
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 05:55:41 -0500

    test: output for area and perimeter

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,5 +3,19 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
+        Rectangle box1 = new Rectangle(5,4);
+
+        System.out.println("get area: " + box1.getArea());
+        System.out.println("perimeter is: " + box1.getPerimeter());
+
+
+        Square box2 = new Square(5);
+
+        System.out.println("Box2 area is: " + box2.getArea());
+        System.out.println("Box 2 perimeter is: " + box2.getPerimeter());
+
+
+
+
     }
 }
