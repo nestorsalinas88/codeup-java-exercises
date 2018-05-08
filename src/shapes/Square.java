3da3296b54828b8c88f8c43500793ff574957189
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 05:25:00 -0500

    created: methods to get are and perimeter

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,13 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle {
+    protected double side;
+
+  public double getArea(){
+      return this.side * this.side;
+  }
+  public double getPerimeter(){
+      return this.side * 4;
+  }
+
 }
