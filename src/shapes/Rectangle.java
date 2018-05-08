29e3f112a3ef17c52d14c4d971458dea36170027
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 14:10:53 -0500

    chore: completed constructor and methods

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,6 +1,6 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral {
 
 //    protected double length;
 //    protected  double width;
@@ -21,4 +21,28 @@ public class Rectangle {
 //    }
 
 
+    public Rectangle(int length, int width) {
+        super(length, width);
+    }
+
+    @Override
+    void setLength(int input) {
+        setLength(input);
+    }
+
+    @Override
+    void setWidth(int input) {
+        setWidth(input);
+
+    }
+
+    @Override
+    public double getPerimeter() {
+        return getLength() * getWidth();
+    }
+
+    @Override
+    public double getArea() {
+        return (getLength()*2) + (getWidth()*2);
+    }
 }
