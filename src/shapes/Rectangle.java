1c34a46a7a98454a935c5f1227b467bf0ceb289f
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 05:54:32 -0500

    refactor code missing THIS keyword

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -5,6 +5,11 @@ public class Rectangle {
     protected double length;
     protected  double width;
 
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
 
 
     public double getArea(){
