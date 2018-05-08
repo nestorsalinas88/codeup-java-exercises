e5f55421a90fd9a4911dd536c1004d89edf3c5ff
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-08 04:40:34 -0500

    create: methods to call double length and width

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,20 @@
 package shapes;
 
 public class Rectangle {
+
+    protected double length;
+    protected  double width;
+
+//    need to get the l and w
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+
 }
