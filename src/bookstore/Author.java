de4954dc50029df5d563f148d572410e68bcf87e
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-09 10:13:48 -0500

    chore: adjusted constructor

--- src/bookstore/Author.java
+++ src/bookstore/Author.java
@@ -3,6 +3,14 @@ package bookstore;
 public class Author extends Person{
 
     private String name;
+    private int number;
+
+
+    public static void numberOfBooks(int number){
+        for (int i = 0; i <= 10; i++) {
+            Math.floor(Math.random());
+        }
+    }
     public Author(String name) {
         super(name);
     }
