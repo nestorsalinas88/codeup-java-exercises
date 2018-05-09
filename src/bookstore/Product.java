303ca8b7b8d986a011cac30e007057806b2d65c2
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-09 10:12:55 -0500

    created: new class with constructor

---
+++ src/bookstore/Product.java
@@ -0,0 +1,14 @@
+package bookstore;
+
+public class Product {
+
+    public String title;
+    public int price;
+
+    public Product(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+
+}
