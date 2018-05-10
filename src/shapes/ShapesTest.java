50e92e5b110dc2c8000f2d6f7e59e73a58e244d5
nestorsalinas88 <nestorasm88@gmail.com>
2018-05-10 16:39:53 -0500

    chore: tested code

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,17 +3,43 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(5,4);
+        Measurable box1 = new Rectangle(5,4);
 
         System.out.println("get area: " + box1.getArea());
         System.out.println("perimeter is: " + box1.getPerimeter());
 
 
-        Square box2 = new Square(5);
+        Measurable box2 = new Square(5);
 
         System.out.println("Box2 area is: " + box2.getArea());
         System.out.println("Box 2 perimeter is: " + box2.getPerimeter());
 
+
+////////////////   NOTES THAT BELONGS IN MAIN METHOD    /////////////////////
+
+
+//    //        implicit casting
+////        converting lower type data into higher type data
+//    int height = 6;
+//    double mass = 220.2345;
+//
+//    double result = height - mass;
+//
+//        System.out.println("this is implicit :" + result);
+//
+//
+//
+////        explicit casting
+////        converting higher data types into lower data types
+//
+//    int j = 100;
+//    byte b;
+//
+//    b = (byte)j;
+//        System.out.println("explicit " + b);
+//
+//
+//}
 
 
 
