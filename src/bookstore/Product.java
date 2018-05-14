18b395409c4aea465cbd517c49b59e139e39f773
nestor salinas <nestorasm88@gmail.com>
2018-05-14 13:57:28 -0500

    chore: trim fat of code

--- src/bookstore/Product.java
+++ src/bookstore/Product.java
@@ -6,6 +6,56 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public abstract class Product {
+    public static Product[] findAll() {
+        return new Product[]{
+                new Book("Book",3,"Title", "names", 45),
+                new Book("Book", 3,"Casablanca", "bleh", 6),
+                new Book("Book", 3,"The Godfather","drama",4),
+                new Book("Book", 3,"Gone With The Wind", "drama", 4),
+                new Book("Book", 3,"Lawrence Of Arabia", "drama", 4),
+                new Book("Book", 3,"The Wizard Of Oz", "musical", 4),
+                new Book("Book", 3,"The Graduate", "drama", 4),
+                new Book("Book", 3,"On The Waterfront", "drama", 4),
+                new Book("Book", 3,"Schindler's List", "drama", 4),
+                new Book("Book", 3,"Singin' In The Rain", "musical", 4),
+                new Book("Book", 3,"It's A Wonderful Life", "drama", 4),
+                new Book("Book", 3,"Sunset Boulevard", "drama", 4),
+                new Book("Book", 3,"The Bridge On The River Kwai", "drama", 4),
+                new Book("Book", 3,"Some Like It Hot", "drama", 4),
+                new Book("Book", 3,"Star Wars", "scifi",4),
+                new Book("Book", 3,"All About Eve", "drama",4),
+                new Book("Book", 3,"The African Queen", "drama",4),
+                new Book("Book", 3,"Psycho", "horror",6),
+                new Book("Book", 3,"Chinatown", "drama",6),
+                new Book("Book", 3,"One Flew Over The Cuckoo's Nest", "drama",5),
+                new Book("Book", 3,"The Grapes Of Wrath", "drama",5),
+                new Book("Book", 3,"2001: A Space Odyssey", "scifi",5),
+                new Book("Book", 3,"The Maltese Falcon", "drama",5),
+                new Book("Book", 3,"Raging Bull", "drama",5),
+                new Book("Book", 3,"E.T. The extra-terrestrial", "scifi",5),
+                new Book("Book", 3,"Dr. Strangelove", "drama",5),
+                new Book("Book", 3,"Bonnie And Clyde", "drama",5),
+                new Book("Book", 3,"Apocalypse Now", "drama",5),
+                new Book("Book", 3,"Mr. Smith Goes to Washington", "drama",5),
+                new Book("Book", 3,"The Treasure Of The Sierra Madre", "drama",5),
+                new Book("Book",4,"Yankee Doodle Dandy", "musical", 5),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+                new Movie("Movie", 5,"Steven Spielberg","kung fu",1993),
+        };
+    }
+
+
 
 // what are we doing with the product?
 // Where are we placing it?
@@ -22,7 +72,7 @@ public abstract class Product {
 
     }
 
-    public static Product[] ProductArray= Arrays.copyOf(ProductInventory.findAll(), ProductInventory.findAll().length);
+    public static Product[] ProductArray= Arrays.copyOf(Product.findAll(), Product.findAll().length);
 
     public static void displayProduct(Product product) {
         System.out.print(product);
@@ -108,6 +158,23 @@ public abstract class Product {
 //    }
 
 
+
+//           ######### Vocabulary Definitions #########
+
+
+//    Class: a blueprint for how a data structure should function
+//
+//    Constructor: instructs the class to set up the initial state of an object
+//
+//            Object: instance of a class that stores the state of a class
+//
+//    Method: set of instructions that can be called on an object
+//
+//    Parameter: values that can be specified when creating an object or calling a method
+//
+//    Return value: specifies the data type that a method will return after it runs
+//
+//    Inheritance: allows one class to use functionality defined in another class
 
 
     public double getPrice() {
