0847125336d9138075f0897cddcbfd978bc3d170
nestor salinas <nestorasm88@gmail.com>
2018-05-14 17:37:48 -0500

    chores: completed grade application

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -10,6 +10,92 @@ public class GradesApplication {
     private static  HashMap<String, Student> students;
 
 
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Grades Application");
+        students = getStudents();
+
+        do {
+            showStudents(students);
+
+            System.out.println("Enter the name for student's grades ");
+            String choice = input.getString();
+
+            if (students.containsKey(choice)){
+                Student student = students.get(choice);
+                System.out.println(student);
+
+                promptForGradeDetail(student);
+
+
+            } else {
+                System.out.println(choice + " was not found in our system.");
+                promptToAddNewStudent();
+            }
+
+
+        } while (input.yesNo("Would you like info on differnt student? press y/n"));
+
+        if (input.yesNo("See all ? y/n ")){
+            showAllStudentsAndGrades(students);
+        }
+        System.out.println("ADIOS");
+    }
+    protected static void showAllStudentsAndGrades(HashMap<String, Student> students) {
+        students.forEach((key, student) -> {
+            System.out.println(key + "'s" + " grade average is a " + student.getGradeAverage());
+        });
+    }
+    protected static void promptToAddNewStudent() {
+        System.out.println("Would you like to add a new student?");
+
+        if(input.yesNo("Press y or yes")) {
+            addStudent(); // addStudent makes a new student object and adds it to the HashMap
+        }
+    }
+
+
+    protected static void promptForGradeDetail(Student student) {
+        if(input.yesNo("Would you like to see all their grades? Yes or y to continue.")) {
+            student.showAllGrades();
+        }
+    }
+    protected static Student makeStudent() {
+        String name = input.getString("What is the student's name? ");
+
+        Student student = new Student(name);
+
+        int grade;
+
+        do {
+            System.out.println("Input a grade for the student");
+
+            grade = input.getInt();
+
+            student.addGrade(grade);
+
+        } while(input.yesNo("Do you want to add more grades for " + name + "?"));
+
+        return student;
+    }
+
+
+
+    protected static void addStudent() {
+        String username = input.getString("What is the student's GitHub username?");
+
+        Student student = makeStudent();
+
+        students.put(username, student); // add the student object w/ their username key to the students HashMap.
+
+    }
+
+    protected static void showStudents(HashMap<String, Student> students){
+        for (String student : students.keySet()){
+            System.out.println(" | " + student);
+        }
+    }
+
     protected static HashMap<String, Student> getStudents(){
         HashMap<String, Student> students = new HashMap<>();
 
